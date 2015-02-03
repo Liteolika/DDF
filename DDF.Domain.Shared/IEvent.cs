7118950f5c6608df60658344797a9d0ee43300be
petcar <peter.carlsson@se.fujitsu.com>
2015-02-03 14:11:26 +0100

    IEvent

---
+++ DDF.Domain.Shared/IEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DDF.Domain.Shared
+{
+    public interface IEvent
+    {
+        Guid AggregateId { get; set; }
+        Guid EventId { get; set; }
+        DateTimeOffset EventTimestamp { get; set; }
+    }
+}
